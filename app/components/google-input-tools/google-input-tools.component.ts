4daa5915ea1669b084d4ff8fcb9af363e0560b86
masiceiu <masiceiu@gmail.com>
2021-02-10 12:40:14 +0600

    2 files modified

--- app/components/google-input-tools/google-input-tools.component.ts
+++ app/components/google-input-tools/google-input-tools.component.ts
@@ -4,37 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { noop, Observable, Observer, of } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 
-interface GitHubUserSearchResponse {
-  total_count: number;
-  incomplete_results: boolean;
-  items: GitHubUser[];
-}
-
-interface GitHubUser {
-  login: string;
-  id:  number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  received_events_url: string;
-  type: string;
-  score: number;
-}
-
 @Component({
   selector: 'app-google-input-tools',
   templateUrl: './google-input-tools.component.html',
   styleUrls: ['./google-input-tools.component.css']
 })
 export class GoogleInputToolsComponent implements OnInit {
-search: string;
-  suggestions$: Observable<GitHubUser[]>;
+  search: string;
+  suggestions$: Observable<string[]>;
   errorMessage: string;
   search_query: string;
 
@@ -53,11 +30,21 @@ search: string;
           /* 
           ["SUCCESS",[["sobhan",["সোবহান","সোভান","শোভন","সোবাহান","শোভান"],[],{"candidate_type":[0,0,0,0,0]}]]]*/
           // using github public api to get users by name
-          return this.http.get<GitHubUserSearchResponse>(
-            'https://inputtools.google.com/request?text=sobhan&&itc=bn-t-i0-und&num=5&cp=0&cs=1&ie=utf-8&oe=utf-8', {
-            params: { q: query }
-          }).pipe(
-            map((data: GitHubUserSearchResponse) => data && data.items || []),
+          return this.http.get<any>(
+            'https://inputtools.google.com/request?text=sobhan&&itc=bn-t-i0-und&num=5&cp=0&cs=1&ie=utf-8&oe=utf-8').pipe(
+            //map((data: any) => data || []),
+            map((data: any) => {
+              if(data.length > 0 && data[1].length > 0 && data[1][0].length > 0 && data[1][0][1].length > 0){
+                var results = data[1][0][1];
+                results[5] = query;
+                /*return of(
+                    results.filter((result: any) => {
+                      return {id:'', suggestion:result};
+                    })
+                  );*/
+                return results;
+              }
+            }),
             tap(() => noop, err => {
               // in case of http error
               this.errorMessage = err && err.message || 'Something goes wrong';

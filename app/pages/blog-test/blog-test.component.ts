746c259a899e062ab2d0940bb152834da89aa631
masiceiu <masiceiu@gmail.com>
2021-02-23 22:12:53 +0600

    2 files modified

--- app/pages/blog-test/blog-test.component.ts
+++ app/pages/blog-test/blog-test.component.ts
@@ -9,18 +9,30 @@ export class BlogTestComponent implements OnInit {
 
   private isSearchActive = true;
   constructor() { }
-  langs: any[] = [
-    { id:'bn', name:'Bangla' },
-    { id:'en', name:'English' },
-    { id:'ar', name:'Arabic' }
-  ];
-  ngOnInit() {
+  log: any = {};
+  langs: any[] = [];
+  ngOnInit(){
+    this.langs = [
+      { id : 'en', name : 'English', ImageUrl : 'https://www.countryflags.io/be/flat/64.png'},
+      { id : 'fr', name : 'French', ImageUrl : 'https://www.countryflags.io/fr/flat/64.png'},
+      { id : 'pl', name : 'Polish', ImageUrl : 'https://www.countryflags.io/pl/flat/64.png'},
+      { id : 'bn', name : 'Bangla', ImageUrl : 'https://www.countryflags.io/bd/flat/64.png'},
+      { id : 'ar', name : 'Arabic', ImageUrl : 'https://www.countryflags.io/sa/flat/64.png'},
+      ];
   }
   searchActiveChange(): void {
     this.isSearchActive = !this.isSearchActive;
   }
+  onMenuItemClick(data: any)
+  {
+    //data.dropdown.autoClose = false;
+    //this.log.menuItem = data;
+    //console.log(data);
+    //console.log(data);
+    //console.log(data.dropdown.autoClose);
+  }
   onOpenChange(data: boolean): void {
-    //this.text = data ? 'opened' : 'closed';
+    //this.log.menuOpen = data;
   }
   handler(value: string): void {
     //this.messages.push(`Event ${value} is fired`);

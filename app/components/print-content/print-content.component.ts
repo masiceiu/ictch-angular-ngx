56490f521ab1b2ece73678451d9c81f932c2466c
masiceiu <masiceiu@gmail.com>
2021-03-14 12:48:20 +0600

    1 file modified

--- app/components/print-content/print-content.component.ts
+++ app/components/print-content/print-content.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrintContentComponent implements OnInit {
 
+  //https://stackblitz.com/edit/angular-nested-components?file=app%2Fapp.module.ts
+  //https://stackblitz.com/edit/container-and-nested-components?file=app%2Fapp.module.ts
+  //https://stackblitz.com/edit/content-nested-components?file=src%2Fapp%2Fapp.module.ts
   constructor() { }
 
   ngOnInit() {

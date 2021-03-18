0ecf286c95774b2af2f3d6b1c3bc9e38371f72cf
masiceiu <masiceiu@gmail.com>
2021-03-18 23:49:49 +0600

    3 files modified

--- app/shared/video-player/video-player-02/video-player-02.component.ts
+++ app/shared/video-player/video-player-02/video-player-02.component.ts
@@ -9,9 +9,63 @@ import { VideoPlayerService } from './video-player.service';
 })
 export class VideoPlayer02Component implements OnInit {
 
-  constructor() { }
+  mediaID: string = "1402726504"; // 1630723954, 2667647842, 1402726504 
+  video: any = {
+    title: "",
+    description: "",
+    duration: "",
+    key: ""
+  }
+  constructor(private videoPlayerService: VideoPlayerService) { }
 
   ngOnInit() {
+    //this.getVideo(this.mediaID);
   }
-  
+  name = 'Video events';
+  videoSource = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
+
+  @ViewChild('videoPlayer') videoplayer: any;
+  public startedPlay:boolean = false;
+  public show:boolean = false;
+  pauseVideo(videoplayer)
+  {
+    videoplayer.nativeElement.play();
+    // this.startedPlay = true;
+    // if(this.startedPlay == true)
+    // {
+      setTimeout(() => 
+      {
+        videoplayer.nativeElement.pause();
+        if(videoplayer.nativeElement.paused)
+        {
+          this.show = !this.show;       
+        } 
+      }, 5000);
+    // }
+  }
+
+  closebutton(videoplayer){
+    this.show = !this.show; 
+    videoplayer.nativeElement.play();
+  }
+
+  getVideo(mediaID: string) {
+    this.videoPlayerService.getVideo(mediaID).subscribe((video: any) => {
+      this.video.title = video.items[0].title; // set title
+      this.video.description = video.items[0].description; // set description
+
+      // set duration
+      let duration = video.items[0].duration;
+      let minutes = Math.floor(duration / 60);
+      let seconds = Math.floor(duration % 60);
+      this.video.duration = minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+
+      this.video.key = video.items[0].assetDescriptors[1].key; // set key video src
+
+      setTimeout(() => {
+        //this.videoPlayer.nativeElement.play()
+        //this.videoPlayer.nativeElement.pause();
+      }, 0); // play
+    })
+  }
 }

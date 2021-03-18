d714c585919d31c6f3c9d73eb7f35b26e000b932
masiceiu <masiceiu@gmail.com>
2021-03-18 22:38:28 +0600

    2 files modified, 6 added

---
+++ app/shared/video-player/video-player-snapshot/video-player-snapshot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-player-snapshot',
+  templateUrl: './video-player-snapshot.component.html',
+  styleUrls: ['./video-player-snapshot.component.css']
+})
+export class VideoPlayerSnapshotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

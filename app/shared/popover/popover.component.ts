291c3365ee6aef9c65c862fb78f97d8dfd67e5c0
masiceiu <masiceiu@gmail.com>
2021-06-21 15:37:12 +0600

    2 files modified

--- app/shared/popover/popover.component.ts
+++ app/shared/popover/popover.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopoverComponent implements OnInit {
 
+  html = `<span class="btn btn-danger">Never trust not sanitized HTML!!!</span>`;
   constructor() { }
 
   ngOnInit() {

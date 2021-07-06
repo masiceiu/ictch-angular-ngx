0b8dc758d4e2d4ae809ed82da2f0f50de3c1e833
masiceiu <masiceiu@gmail.com>
2021-07-06 10:43:07 +0600

    5 files added

---
+++ environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  url: {
+    getStudentList: './assets/json/get-student-list.json',
+    getTeacherList: './assets/json/get-teacher-list.json',
+    getDistrictList: './assets/json/get-district-list.json'
+  }
+};

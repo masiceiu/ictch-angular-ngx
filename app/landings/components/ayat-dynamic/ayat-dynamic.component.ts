807b6811d646b891c5c689c176a13a5304df7453
masiceiu <masiceiu@gmail.com>
2021-07-07 11:31:48 +0600

    2 files modified, 3 added

---
+++ app/landings/components/ayat-dynamic/ayat-dynamic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ayat-dynamic',
+  templateUrl: './ayat-dynamic.component.html',
+  styleUrls: ['./ayat-dynamic.component.css']
+})
+export class AyatDynamicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- app/landings/landings.module.ts
+++ app/landings/landings.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LandingsComponent } from "./landings.component";
 import { LandingsRoutingModule } from "./landings-routing.module";
+import { AyatDynamicComponent } from './components/ayat-dynamic/ayat-dynamic.component';
 
 @NgModule({
   imports: [
     CommonModule, 
     LandingsRoutingModule
   ],
-  declarations: [LandingsComponent]
+  declarations: [LandingsComponent, AyatDynamicComponent]
 })
 export class LandingsModule {}

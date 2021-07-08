dc1da07cfc1afc927fb34b474cca17a8bbd2cc1a
masiceiu <masiceiu@gmail.com>
2021-07-08 12:34:48 +0600

    2 files modified

--- app/shared/components/add-btn-fixed/add-btn-fixed.component.ts
+++ app/shared/components/add-btn-fixed/add-btn-fixed.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-btn-fixed',
@@ -20,6 +20,40 @@ export class AddBtnFixedComponent implements OnInit {
   addClick(data:any) 
   {
     this.onAddClick.emit(data||{});
+    this.scrollTo();
   }
   //https://stackblitz.com/edit/ngstyle-examples?file=src%2Fapp%2Fapp.component.html
+/***************************************/
+  @HostListener("window:scroll") onWindowScroll() {
+    /*let scroll = window.pageYOffset ||
+                document.documentElement.scrollTop ||
+                document.body.scrollTop || 0;
+
+    const max = document.documentElement.scrollHeight -
+                document.documentElement.clientHeight;
+
+    if (scroll === max) {
+      alert('Bottom');
+    }*/
+  }
+  scrollTo(input:any) 
+  { 
+    switch(input){
+      case"top":
+        (function smoothscroll(){ 
+          var currentScroll = document.documentElement.scrollTop || document.body.scrollTop; 
+          if (currentScroll > 0) 
+          {
+            window.requestAnimationFrame(smoothscroll);
+            window.scrollTo(0, currentScroll - (currentScroll / 5));
+          }
+        })();
+        break;
+        case"bottom":
+        (function smoothscroll(){ 
+          window.scrollTo(0,document.body.scrollHeight);
+        })();        
+        break;
+    } 
+  }
 }

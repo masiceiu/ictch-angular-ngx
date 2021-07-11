f1381f7da9610587f234bad6dd7ff58dc1e67730
masiceiu <masiceiu@gmail.com>
2021-07-11 14:50:55 +0600

    3 files modified

--- app/shared/components/nav-carousel/nav-carousel.component.ts
+++ app/shared/components/nav-carousel/nav-carousel.component.ts
@@ -1,30 +1,102 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-carousel',
   templateUrl: './nav-carousel.component.html',
   styleUrls: ['./nav-carousel.component.css']
 })
-export class NavCarouselComponent implements OnInit {
+export class NavCarouselComponent implements OnInit, AfterViewInit {
+  
+  size:any='';
+  constructor() {}
 
-  itemsPerSlide = 8;
-  singleSlideOffset = true;
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  @HostListener('window:resize', [])
+  onResize() {
+    this.detectScreenSize('hos:');
+  }
+
+  ngAfterViewInit() {
+    this.detectScreenSize('init:');
+  }
+
+  detectScreenSize(log:any) {
+    (function smoothscroll() {
+      let res = {
+        width: window.screen.width
+      };
+      //window.screen.width
+      /*outerWidth*/
+      if (res.width < 576) {
+        this.size = 'Extra small';
+      }
+      if (res.width >= 576) {
+        this.size = 'Small';
+      }
+      if (res.width >= 768) {
+        this.size = 'Medium';
+      }
+      if (res.width >= 992) {
+        this.size = 'Large';
+      }
+      if (res.width >= 1200) {
+        this.size = 'Extra Large';
+      }
+      console.log(log,this.size);
+    })();
   }
 
   slides = [
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/3.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/4.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/5.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/6.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/7.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/8.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg'},
-    {image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg'}
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/3.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/4.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/5.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/6.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/7.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/8.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg'
+    },
+    {
+      text: 'Item',
+      image:
+        'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg'
+    }
   ];
-
 }

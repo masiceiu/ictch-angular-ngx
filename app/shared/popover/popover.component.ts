ce29ebdbd62dbb5543fa1e3dc2afa6f6abe4d952
masiceiu <masiceiu@gmail.com>
2021-07-28 18:41:05 +0600

    2 files modified

--- app/shared/popover/popover.component.ts
+++ app/shared/popover/popover.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-popover',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopoverComponent implements OnInit {
 
+  @Input() title:any="";//Popover&nbsp;Title
   html = `<span class="btn btn-danger">Never trust not sanitized HTML!!!</span>`;
   constructor() { }
 

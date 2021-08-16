5c7edfe74b0ade507868e629437b2c8b650700d5
masiceiu <masiceiu@gmail.com>
2021-08-16 15:37:43 +0600

    4 files added

---
+++ app/shared/views/ilm-channel/ilm-channel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ilm-channel',
+  templateUrl: './ilm-channel.component.html',
+  styleUrls: ['./ilm-channel.component.css']
+})
+export class IlmChannelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ app/shared/views/ilm-channel/ilm-channel.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IlmChannelComponent } from './ilm-channel.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    IlmChannelComponent
+  ],
+  declarations: [
+    IlmChannelComponent
+  ]
+})
+export class IlmChannelModule { }

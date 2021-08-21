adba246940b0782a2341134f068263fe49e46a96
masiceiu <masiceiu@gmail.com>
2021-08-21 15:17:00 +0600

    2 files modified, 1 added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,39 +1,10 @@
-import { AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
-import { ResizeService } from './core/services/config/resize.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
-
-  constructor(private cdRef: ChangeDetectorRef, private resizeService: ResizeService) { 
-    /* Test */
-    //this.resizeService.resize
-      //.subscribe(x => {
-        //this.size = x;
-     // });
-  }
-
-  ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-    //this.detectScreenSize('init:');
-    //this.cdRef.detectChanges();
-  }
-  @HostListener('window:resize', [])
-  onResize() {
-    //this.detectScreenSize('hos:');
-  }
-  detectScreenSize(log: any) {
-    (function detectChanges(self) {
-      let size = {
-        width: window.innerWidth,
-        height: window.innerHeight
-      };
-      self.resizeService.onResize(size);
-    })(this);
-  }
+export class AppComponent {
+  title = 'life-whois';
 }

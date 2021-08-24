ec68a0aa668a72b0f476346cbbddac95f7ea14ca
masiceiu <masiceiu@gmail.com>
2021-08-24 10:25:15 +0600

    3 files modified

--- app/components/dynamic-input/input-checkbox.ts
+++ app/components/dynamic-input/input-checkbox.ts
@@ -2,4 +2,13 @@ import { InputBase } from './input-base';
 
 export class CheckBoxInput extends InputBase<string> {
   controlType = 'checkbox';
+  type: 'checkbox';
+  //checked: boolean;
+  /*
+  constructor(options) {
+    super(options);
+    this.type = options.type || '';
+    this.checked = options.checked || false;
+  }
+  */
 }

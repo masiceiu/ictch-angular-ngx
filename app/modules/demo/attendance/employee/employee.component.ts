95c9d67656d7eae2dbe54d24c47b56c459137fb9
masiceiu <masiceiu@gmail.com>
2021-09-15 23:46:01 +0600

    2 files modified

--- app/modules/demo/attendance/employee/employee.component.ts
+++ app/modules/demo/attendance/employee/employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import moment from 'moment';
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -7,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  startDate: moment.Moment = moment('1/1/2017')
+  currentDate: moment.Moment = moment();
+  currentTime: string = moment().format('M/D/YYYY hh:mm:ss a');
+  daysFrom2017: number;
+  humanized: string;
+  humanizedNow: string;
+  weeks: number;
 
+  constructor() {
+    this.humanized = moment.duration(moment().diff(this.startDate)).humanize();
+    this.humanizedNow = moment.duration(moment().diff(moment())).humanize();
+
+    // if you need to force to number of days
+    this.daysFrom2017 = this.currentDate.diff(moment('1/1/2017'), 'days');
+
+    // if you need to force to number of weeks
+    this.weeks = moment().diff(this.startDate, 'week');
+    
+    console.log('Days from 1/1/2017:', this.daysFrom2017);
+    console.log('Current Time:', this.currentTime);
+  }
   ngOnInit() {
   }
+  
 }
 //https://stackblitz.com/edit/angular-moment-example
 //https://stackblitz.com/edit/angular-attendance-mock

14715c149b98282ac08381b14e5536588315488c
masiceiu <masiceiu@gmail.com>
2021-09-16 12:52:34 +0600

    2 files modified

--- app/modules/demo/attendance/attendance-routing.module.ts
+++ app/modules/demo/attendance/attendance-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AttendanceComponent } from './attendance.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 const routes: Routes = [{
-  path: "",
+  path: "1",
   component: AttendanceComponent
 },{
-  path: "a1",
-  component: AttendanceComponent
+  path: "",
+  component: EmployeeComponent
 }];
 
 @NgModule({
--- app/modules/demo/attendance/attendance.module.ts
+++ app/modules/demo/attendance/attendance.module.ts
@@ -4,6 +4,13 @@ import { FormsModule } from "@angular/forms";
 
 import { AttendanceComponent } from './attendance.component';
 import { AttendanceRoutingModule } from './attendance-routing.module';
+import { TrackingService } from './employee/tracking.service';
+import { TitlePipe } from './employee/list/title.pipe';
+import { EmployeeComponent } from './employee/employee.component';
+import { AlertComponent } from './employee/alert/alert.component';
+import { RegisterComponent } from './employee/register/register.component';
+import { EmployeeListComponent } from './employee/list/list.component';
+import { EmployeeCountComponent } from './employee/count/count.component';
 
 @NgModule({
   imports: [

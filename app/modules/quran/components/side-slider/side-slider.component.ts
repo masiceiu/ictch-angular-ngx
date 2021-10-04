11d3e90948ef9ddf54ad9d7f553b0bf4e8f45519
masiceiu <masiceiu@gmail.com>
2021-10-04 16:21:59 +0600

    4 files modified

--- app/modules/quran/components/side-slider/side-slider.component.ts
+++ app/modules/quran/components/side-slider/side-slider.component.ts
@@ -9,6 +9,8 @@ export class SideSliderComponent implements OnInit {
 
   toggler:boolean = true;//style="top: 34px;"
   @Input() positionTop = 0;
+  @Input() icon = "";
+  @Input() iconMarginTop = 0;
   @Input() refTemplate: TemplateRef<any>;
   /*@ViewChild('templateRef', { read: TemplateRef }) templateRef:TemplateRef<any>;
   constructor(private vref:ViewContainerRef) {

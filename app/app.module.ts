978859ab47c94fe35c1871f9e489492749b5828d
masiceiu <masiceiu@gmail.com>
2021-11-06 11:54:33 +0600

    2 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 

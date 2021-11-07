92635b4b260e6846ff44a217bdec227746bdfb0e
masiceiu <masiceiu@gmail.com>
2021-11-07 12:49:52 +0600

    4 files modified

--- app/modules/quran/modules/ayat/ayat-search/ayat-content/ayat-content.component.ts
+++ app/modules/quran/modules/ayat/ayat-search/ayat-content/ayat-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-ayat-content',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AyatContentComponent implements OnInit {
 
+  @Input() input:any = {};
+  @Output() onItemClick = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit() {

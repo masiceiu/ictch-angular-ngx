a832653e90d176373c0763ada8b576dea50b47d6
masiceiu <masiceiu@gmail.com>
2021-11-08 16:08:49 +0600

    3 files modified

--- app/modules/quran/modules/ayat/ayat-search/ayat-content/ayat-content.component.ts
+++ app/modules/quran/modules/ayat/ayat-search/ayat-content/ayat-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-ayat-content',
@@ -7,12 +7,13 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class AyatContentComponent implements OnInit {
   
-  @Input() it:any = {};
+  //@Input() it:any = {};
   @Input() data:any = {};
   @Input() config:any = {};
   @Input() isSuraSearch = false;
-  @Input() suraIndex:any = {1:{name:"aa"}};
-  @Input() star_ayats:any[] = [];
+  //@Input() suraIndex:any = {1:{name:"aa"}};
+  //@Input() star_ayats:any[] = [];
+  @Input() refMore: TemplateRef<any>;
   @Output() onItemClick = new EventEmitter<any>();
   constructor() { }
 

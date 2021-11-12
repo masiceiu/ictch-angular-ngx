5de3a0b055599c69e25c93505164c2be8093844b
masiceiu <masiceiu@gmail.com>
2021-11-12 11:03:07 +0600

    3 files modified

--- app/modules/quran/modules/ayat/ayat-search/ayat-content/ayat-content.component.ts
+++ app/modules/quran/modules/ayat/ayat-search/ayat-content/ayat-content.component.ts
@@ -7,28 +7,33 @@ import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@an
 })
 export class AyatContentComponent implements OnInit {
   
-  //@Input() it:any = {};
   @Input() data:any = {};
   @Input() config:any = {};
-  //@Input() isSuraAllow = false;
-  //@Input() suraIndex:any = {1:{name:"aa"}};
-  //@Input() star_ayats:any[] = [];
-  @Input() refMore: TemplateRef<any>;
+  @Output() click = new EventEmitter<any>();
   @Output() onItemClick = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit() {
-    
+  }
+  _onClick(ev:any){
+    ev.preventDefault();
+    this.click.emit(ev);
   }
   _onItemClick(data:any, switch_on:string){
     //console.log(data,switch_on);
     data.$event.preventDefault();
     this.onItemClick.emit({data,switch_on});
   }
+  /*
+  @Input('data')
+  set in(val:any) {
+    this.data = val;
+  }
+  
   _isWithSuraView:boolean = false;
   get isWithSuraView() { return this.config.isSuraSearch && this._isWithSuraView }
   @Input('isWithSuraView')
   set in(val:boolean) {
     this._isWithSuraView = val;
-  }
+  }*/
 }

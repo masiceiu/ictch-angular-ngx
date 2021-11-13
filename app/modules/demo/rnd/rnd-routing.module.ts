8e73ced45f8a2bf1caf43ffbb961857c26e141bb
masiceiu <masiceiu@gmail.com>
2021-11-13 14:44:08 +0600

    1 file modified

--- app/modules/demo/rnd/rnd-routing.module.ts
+++ app/modules/demo/rnd/rnd-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RndComponent } from './rn.component';
+import { RndComponent } from './rnd.component';
 
 const routes: Routes = [{
   path: "",

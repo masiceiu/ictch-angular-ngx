5d11ad74d3a1d026121d80959ab702bdc4db5a02
masiceiu <masiceiu@gmail.com>
2021-11-24 15:26:33 +0600

    1 file modified

--- app/settings.service.ts
+++ app/settings.service.ts
@@ -11,7 +11,7 @@ export class SettingsService {
   private _config:any
   constructor(private http: HttpClient) { }
   load(): any {
-    this.http.get<any>('./../assets/config.json').subscribe(res =>
+    this.http.get<any>('assets/config.json').subscribe(res =>
       {
         this._config = res;
       });

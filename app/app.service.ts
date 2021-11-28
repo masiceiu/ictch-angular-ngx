e2d6e0015b514a5dd08253118056c10691634aa6
Abdus.Sobhan <masiceiu@gmail.com>
2021-11-28 16:28:33 +0600

    28112021

--- app/app.service.ts
+++ app/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AppService {
@@ -7,5 +8,10 @@ export class AppService {
   public get data() { return this._subject.asObservable();}
   public update(obj:object) { this._subject.next(obj);}
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.http.get<any>('/shared/data/config.json').subscribe(res =>
+      {
+        console.log(res);
+      });
+  }
 }

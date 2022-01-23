3ccf0ace75348970df420d7d1c1366d70f452394
masiceiu <masiceiu@gmail.com>
2022-01-23 15:35:07 +0600

    1 file modified

--- app/modules/demo/rnd/sse/sse.service.ts
+++ app/modules/demo/rnd/sse/sse.service.ts
@@ -12,8 +12,12 @@ export class SseService {
     observeMessages(sseUrl: string): Observable<any> {
         return new Observable<any>(obs => {
             const es = new EventSource(sseUrl);
+            //console.log(es);
             es.addEventListener('message', (evt) => {
                 obs.next(evt.data);
+                //console.log(es.withCredentials);
+                //console.log(es.readyState);
+                //console.log(es.url);
                 //console.log(evt.data);
             });
             return () => es.close();
